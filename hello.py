97dcf935bd1a4fc5cb5639124550d6714fa68f3a
Dilki__MMM <y.dil2003@gmail.com>
2026-03-08 09:11:22 +0530

    Update hello.py with goodbye function

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 print("Hello, Git!")
 print("This is my first Python file in a branch")
+print("I've  updated this file")
 
 def greet(name):
     return f"Hello, {name}!"
+
+def goodbye(name):
+    return f"Goodbye, {name}! See you soon!"
+
+print(greet("Student"))
+print(goodbye("Friend"))
 
 print(greet("Student"))
